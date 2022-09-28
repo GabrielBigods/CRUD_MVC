5163ab479c4945a9608243c7764d4377eed711ca
Gabriel <gabrielrdgues@gmail.com>
2022-09-27 23:31:20 -0300

    second commit

---
+++ CrudMvc/CrudMvc/Migrations/20220927212136_Inicial-criacao.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CrudMvc.Migrations
+{
+    public partial class Inicialcriacao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cliente",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CPF = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RG = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Rua = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Numero = table.Column<int>(type: "int", nullable: false),
+                    Bairro = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cidade = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cliente");
+        }
+    }
+}
---
+++ CrudMvc/CrudMvc/Models/Cliente.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CrudMvc.Models
+{
+
+    [Table("Cliente")]
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string CPF { get; set; }
+        
+        public string RG { get; set; }
+
+        public string Rua { get; set; }
+
+        public int Numero { get; set; }
+
+        public string Bairro { get; set; }  
+
+        public string Cidade { get; set; }
+
+
+
+    }
+}
